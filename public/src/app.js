be4f81881b652eaeb4b4822f239f4114b7fa4f59
Little Bit <cthibodeau09@gmail.com>
2023-07-18 17:23:21 -0400

    added imports to nav bar locations

--- public/src/app.js
+++ public/src/app.js
@@ -1,4 +1,8 @@
 import React from "react";
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
+import MyWork from "./components/MyWork";
+import Contact from "./components/Contact";
 
 export default function App() {
     return (
