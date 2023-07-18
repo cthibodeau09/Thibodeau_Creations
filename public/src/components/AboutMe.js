99b6f68434f4862c3687a91313c445741388a6cc
Little Bit <cthibodeau09@gmail.com>
2023-07-18 17:45:01 -0400

    updated text in about me

--- public/src/components/AboutMe.js
+++ public/src/components/AboutMe.js
@@ -6,14 +6,9 @@ export default function AboutMe() {
              <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            Hi, I'm Courtney!
           </h1>
-          <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+          <p className="mb-8 leading-relaxed">I am a student in the MSU coding bootcamp. Years ago I was enrolled in a Web Development program at Baker College, but they cancelled my program half way through and I then decided to change my career path. I am currently a CNA at a nursing and rehab center, and have been working in healthcare for 5 years. I recently injured my shoulder at work and felt that I cannot continue the heavy workload the rest of my life and wanted to get back into something that I enjoyed and could do from home, so here I am!
           </p>
           <div className="flex justify-center">
             <a
